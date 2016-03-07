2aec577268d86ab6acb84de25db74252c64af67d
EtienneTr <supinfo@Supinfos-Mac.local>
2016-03-07 10:37:51 +0100

    new project swift

---
+++ app.swift/appswift/appswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appswift
+//
+//  Created by Supinfo on 07/03/16.
+//  Copyright © 2016 Supinfo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
