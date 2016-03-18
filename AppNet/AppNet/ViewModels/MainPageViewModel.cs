6df10fbc6b7aacbe5d2b29602404250139a93ee6
IhabON <ihab7@hotmail.fr>
2016-03-18 09:45:35 +0100

    Correction

--- AppNet/AppNet/ViewModels/MainPageViewModel.cs
+++ AppNet/AppNet/ViewModels/MainPageViewModel.cs
@@ -46,10 +46,11 @@ namespace AppNet.ViewModels
         
         public async void TweetLogin()
         {
+        	var appCredential = new TwitterCredentials("HDtWgQ90KCQUgfF8yhpQLxj5U", "J4yg5hVrlvCocWoT4lWCqQXvaZ7C5YAfw9wgGZwZF5YuFY46Up");
         	
             if (!string.IsNullOrEmpty(_codeinput))
             {
-                var userCredentials = CredentialsCreator.GetCredentialsFromVerifierCode(Codeinput, Connexion.GetAppCredentials());
+                var userCredentials = CredentialsCreator.GetCredentialsFromVerifierCode(Codeinput, appCredential);
                 Auth.SetCredentials(userCredentials);
                 
                 if (userCredentials != null)
