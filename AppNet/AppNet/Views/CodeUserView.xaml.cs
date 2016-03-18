425a267a07a3f68cf1097c64d8c93d51b403674d
IhabON <ihab7@hotmail.fr>
2016-03-18 09:39:45 +0100

    CodeUserView

--- AppNet/AppNet/Views/CodeUserView.xaml.cs
+++ AppNet/AppNet/Views/CodeUserView.xaml.cs
@@ -7,20 +7,31 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Core;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 using Tweetinvi;
 using AppNet.ViewModels;
 using AppNet.Model;
 using Windows.UI.Xaml.Navigation;
+using Tweetinvi.Core.Credentials;
 
 namespace AppNet.Views
 {
 	/// <summary>
 	/// Interaction logic for CodeUserView.xaml
 	/// </summary>
-	public partial class CodeUserView : Page
+	public sealed partial class CodeUserView : Page
 	{
 		public CodeUserView()
 		{
@@ -29,8 +40,9 @@ namespace AppNet.Views
 		
 		protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+        	var appCredential = new TwitterCredentials("HDtWgQ90KCQUgfF8yhpQLxj5U", "J4yg5hVrlvCocWoT4lWCqQXvaZ7C5YAfw9wgGZwZF5YuFY46Up");
  
-        	var url = CredentialsCreator.GetAuthorizationURL(Connexion.GetAppCredentials());
+        	var url = CredentialsCreator.GetAuthorizationURL(appCredential);
 
             this.TwitterPin.Source = url;
         	
